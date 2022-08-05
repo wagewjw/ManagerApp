4d4d55e75616fd8e1e06165414745d8068c2e1d9
wage <wagewangjiawei@outlook.com>
2022-08-05 22:56:12 +0800

    练习1

--- src/main/java/com/wage/managerapp/controller/IndexController.java
+++ src/main/java/com/wage/managerapp/controller/IndexController.java
@@ -1,13 +1,51 @@
 package com.wage.managerapp.controller;
 
+import com.wage.managerapp.entry.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/login")
+    /**
+     * 去login.html
+     * @return
+     */
+    @GetMapping(value = {"/","/login"})
     public String loginPage(){
         return "login";
     }
+
+    @PostMapping("/login")
+    public String main(User user, HttpSession httpSession, Model model){
+        if((!StringUtils.isEmpty(user.getUserName()))&&"123456".equals(user.getPassword())){
+            httpSession.setAttribute("loginUser",user);
+            return "redirect:/main.html";
+        }else {
+            model.addAttribute("msg","账号密码错误");
+            return "login";
+        }
+        //登录成功重定向到main.html->重定向防止页面重复提交
+    }
+
+    /**
+     * 去main.html
+     * @return
+     */
+    @GetMapping("/main.html")
+    public String mainPage(HttpSession httpSession,Model model){
+        Object loginUser=httpSession.getAttribute("loginUser");
+        if(loginUser!=null){
+            return "main";
+        }else {
+            model.addAttribute("msg","请重新登录");
+            return "login";
+        }
+    }
+
 }
---
+++ src/main/java/com/wage/managerapp/controller/TableController.java
@@ -0,0 +1,43 @@
+package com.wage.managerapp.controller;
+
+import com.wage.managerapp.entry.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class TableController {
+
+    @GetMapping("/basic_table")
+    public String basic_table(){
+        return "table/basic_table";
+    }
+
+    @GetMapping("/dynamic_table")
+    public String dynamic_table(Model model){
+        List<User> users=Arrays.asList(new User("dahu","1312"),
+                new User("fafa","dasfas"),
+                new User("gfa","dfawfgadg"));
+        model.addAttribute("users",users);
+        return "table/dynamic_table";
+    }
+
+    @GetMapping("/editable_table")
+    public String editable_table(){
+        return "table/editable_table";
+    }
+
+    @GetMapping("/pricing_table")
+    public String pricing_table(){
+        return "table/pricing_table";
+    }
+
+    @GetMapping("/responsive_table")
+    public String responsive_table(){
+        return "table/responsive_table";
+    }
+
+}
